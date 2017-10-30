51c22f0b1e0c467a2808b2e93b2bc37460a8f731
Marcin Kobiela <kobsonauta@89-64-144-145.dynamic.chello.pl>
2017-10-30 13:37:04 +0100

    Added Auto Layout.

--- Dicee/ViewController.swift
+++ Dicee/ViewController.swift
@@ -9,17 +9,18 @@
 import UIKit
 
 class ViewController: UIViewController {
-    
+
+    var diceArray : Array = [""]
     var randomDiceIndex1: Int = 0
     var randomDiceIndex2: Int = 0
-    
-    let diceArray = ["dice1", "dice2", "dice3", "dice4", "dice5", "dice6"]
+
     
     @IBOutlet weak var diceImageView1: UIImageView!
     @IBOutlet weak var diceImageView2: UIImageView!
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        diceArray = ["dice1", "dice2", "dice3", "dice4", "dice5", "dice6"]
         rollTheDice()
     }
     
